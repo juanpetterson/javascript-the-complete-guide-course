54123c0e9bdad4ea023617a9027d19dfce57f9ab
juanpetterson <juan.petterson@gmail.com>
2020-01-30 07:35:52 -0300

    Finished promise and async module, implemented custom async functions reviewed Promise events

--- 17 Async JavaScript Promises & Callbacks/app.js
+++ 17 Async JavaScript Promises & Callbacks/app.js
@@ -7,7 +7,9 @@ const getPosition = opts => {
       success => {
         resolve(success);
       },
-      error => {},
+      error => {
+        reject(error);
+      },
       opts
     );
   });
@@ -26,25 +28,67 @@ const setTimer = duration => {
 };
 
 function trackUserHandler() {
-  let positionData;
+  // let positionData;
   getPosition()
-    .then(posData => {
-      positionData = posData;
-      // console.log(posData);
-      return setTimer(2000);
-    })
+    .then(
+      posData => {
+        positionData = posData;
+        // console.log(posData);
+        return setTimer(2000);
+      },
+      err => {
+        console.log(err);
+      }
+    )
+    .catch(err => {
+      console.log(err);
+      return 'on we go';
+    }) //only catch the above errors, the 'then' below will execute normally and it not cancel the promise
     .then(data => {
       console.log(data, positionData);
+    })
+    .finally(() => {
+      console.log('This is the Finally');
     });
 
   setTimer(1000).then(() => {
     console.log('Timer done');
-  }); //execute after the above line because its an async function and need to wait
+  }); //execute after the below line because its an async function and need to wait
   //for Message Queue(what is waiting for an empty stack to execute) and the event loop
   console.log('Getting position...');
 }
 
-button.addEventListener('click', trackUserHandler);
+async function trackUserHandlerAsync() {
+  let posData;
+  let timerData;
+
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+
+  setTimer(1000).then(() => {
+    console.log('Timer done');
+  });
+
+  console.log(timerData, posData);
+}
+
+button.addEventListener('click', trackUserHandlerAsync);
+
+// Promise.race([getPosition(), setTimer(1000)]).then(data => {
+//   console.log(data);
+// });
+
+// Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+//   console.log(promiseData);
+// }); // if one promise fails, all the others will not execute
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+}); // execute all promises even if one fails
 
 // let result = 0;
 // for (let i = 0; i < 1000000000; i++) {
