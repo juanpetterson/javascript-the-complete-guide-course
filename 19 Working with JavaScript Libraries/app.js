8e26cbe0c62df55118a723d5a7854157ec4fc90c
juanpetterson <juan.petterson@gmail.com>
2020-01-30 23:19:49 -0300

    Using third party libraries and how to find them

---
+++ 19 Working with JavaScript Libraries/app.js
@@ -0,0 +1,9 @@
+const axios = require('axios').default;
+
+const customers = ['Max', 'Manuel', 'Anna'];
+
+const activeCustomers = ['Max', 'Manuel'];
+
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log(inactiveCustomers);
